49e521f1a73fe96c5ae245703f5055ff593beed7
Alexandre Milim <124930757+Alexandre-Milim@users.noreply.github.com>
2024-02-25 20:34:23 -0300

    Update main.py

--- main.py
+++ main.py
@@ -22,15 +22,14 @@ class Client:
         elif filme in self.listBasic:
             print(f"Assistindo {filme} - Plano: {self.plan}")
         else:
-            upgrade_option = input(f"Filme não disponível para o plano {self.plan}. Deseja fazer um upgrade?")
-            if upgrade_option.lower == "Sim":
-                new_plan = input ("Premium/Intermediário/Basica")
+            upgrade_option = input(f"Filme não disponível para o plano {self.plan}. Deseja fazer um upgrade? (Sim/Não): ")
+            if upgrade_option.lower() == "sim":
+                new_plan = input("Escolha o novo plano (Premium/Basic/Free): ")
                 self.mudar_plan(new_plan)
                 print(f"Upgrade realizado! Agora você está no plano {self.plan}.")
                 print(f"Agora você pode assistir {filme}.")
             else:
                 print("Upgrade não realizado. Filme indisponível.")
-            
 
 cliente = Client("alexandre", "Basic")
 cliente.filmes('avengers')
