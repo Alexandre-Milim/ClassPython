7166f8f43fb9c496b9b5d4039933e11f9affac3a
Alexandre Milim <124930757+Alexandre-Milim@users.noreply.github.com>
2024-02-25 20:05:15 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,10 +1,27 @@
-import flet as ft
+class Client:
+    def __init__(self, name, plan):
+        self.name = name
+        self.list_plan = ["Premium", "Basic"]
+        if plan in self.list_plan:
+            self.plan = plan
+        else:
+            raise Exception("Plano Inválido!")
 
+    def mudar_plan(self, new_plan):
+        if new_plan in self.list_plan:
+            self.plan = new_plan
+        else:
+            raise Exception("Plano Inválido!")
 
-def main(page):
-    field = ft.TextField(
-        label="Enter your PhoneNumber:",
-    )
-    page.add(field)
+    def filmes(self, filme):
+        self.listPremium = ['bee-moovie', 'avengers', 'tinker bell']
+        self.listBasic = ['batman', 'sem floresta']
 
-ft.app(main)
+        if filme in self.listBasic or filme in self.listPremium:
+            print(f"Assistindo {filme} - Plano: {self.plan}")
+        else:
+            print(f"Filme não disponível para o plano {self.plan}")
+
+cliente = Client("alexandre", "Premium")
+cliente.filmes('batman')
+
